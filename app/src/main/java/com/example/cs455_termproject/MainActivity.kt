574c72b22714855dedd6db87bebb13cd3de740d3
Shawn Fernandes <shawnfernandes0912@gmail.com>
2021-04-11 12:41:13 -0600

    changed element Id

--- app/src/main/java/com/example/cs455_termproject/MainActivity.kt
+++ app/src/main/java/com/example/cs455_termproject/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.cs455_termproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var hButton: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hButton = findViewById(R.id.H)
+
     }
 }
